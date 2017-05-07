ef7582d055232ec0babc6a13408f1134093a870b
Erico Silva <augusto.ericosilva@gmail.com>
2017-05-07 16:17:07 -0300

    Add missing file

--- src/main/groovy/com/potatobots/facebook/Application.groovy
+++ src/main/groovy/com/potatobots/facebook/Application.groovy
@@ -1,14 +1,17 @@
 package com.potatobots.facebook
 
 import io.vertx.core.Vertx
+import org.apache.logging.log4j.LogManager
 
 class Application {
+
+    static final LOGGER = LogManager.getLogger Application
 
     static main(args) {
         Vertx vertx = Vertx.vertx()
         vertx.with {
             deployVerticle(ServerVerticle.name) {
-                println 'Server verticle deployed'
+                LOGGER.info 'Server verticle deployed'
             }
         }
     }
